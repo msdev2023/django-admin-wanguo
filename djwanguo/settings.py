b62ccb711a32a3d1bd11a9669d28660510b91fa3
msdev2023 <msdev2023@outlook.com>
2023-03-17 10:32:55 +0800

    add Django i18n settings
    - https://docs.djangoproject.com/en/4.1/topics/i18n/translation/#how-django-discovers-language-preference
    - https://docs.djangoproject.com/en/4.1/topics/i18n/translation/#how-django-discovers-translations

--- djwanguo/settings.py
+++ djwanguo/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -115,6 +116,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
 
 
 # Static files (CSS, JavaScript, Images)
